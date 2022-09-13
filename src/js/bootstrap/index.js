d9c40308fe460e7e411d17d6a613cee9c2e034a7
Gosha Khidzhakadze <gosha@etheric.agency>
2022-09-13 15:17:35 +0700

    serviceworker

--- src/js/bootstrap/index.js
+++ src/js/bootstrap/index.js
@@ -1,3 +1,17 @@
 import './vendor';
 import './init';
 import './utils/parser';
+
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker.register("./sw.js").then(
+            function (registration) {
+                // Registration was successful
+            },
+            function (err) {
+                // registration failed :(
+            }
+        );
+    });
+}
