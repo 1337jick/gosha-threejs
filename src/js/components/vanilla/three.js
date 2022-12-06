aed729127075e1813f1adb127f9c1bac6ac38924
Gosha Khidzhakadze <gosha@etheric.agency>
2022-12-06 15:25:55 +0400

    scroll-based anime

--- src/js/components/vanilla/three.js
+++ src/js/components/vanilla/three.js
@@ -2,183 +2,246 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import * as dat from 'lil-gui';
+import gsap from 'gsap';
+import ScrollSmoother from 'gsap/ScrollSmoother';
 
 export default (element) => {
-    function init() {
-        const parameters = {
-            count: 100000,
-            size: 0.01,
-            radius: 5,
-            branches: 3,
-            spin: 1,
-            randomness: 0.5,
-            randomnessPower: 3,
-            insideColor: '#ff6030',
-            outsideColor: '#1b3984'
-        };
 
-        const gui = new dat.GUI();
-        const canvas = document.querySelector('canvas.js-scene');
-        const scene = new THREE.Scene();
-        const textureLoader = new THREE.TextureLoader();
-
-
-        let geometry = null
-        let material = null
-        let points = null
-
-
-        /**
-         * Galaxy
-         */
-        const generateGalaxy = () => {
-
-            const colorInside = new THREE.Color(parameters.insideColor)
-            const colorOutside = new THREE.Color(parameters.outsideColor)
-
- 
-             // Destroy old galaxy
-            if(points !== null)
-            {
-                geometry.dispose()
-                material.dispose()
-                scene.remove(points)
-            }
-            /**
-             * Geometry
-             */
-            geometry = new THREE.BufferGeometry();
-
-            const positions = new Float32Array(parameters.count * 3);
-            const colors = new Float32Array(parameters.count * 3)
-
-            for (let i = 0; i < parameters.count; i++) {
-                const i3 = i * 3;
-
-                const radius = Math.random() * parameters.radius
-                const spinAngle = radius * parameters.spin;
-                const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2
-                const mixedColor = colorInside.clone()
-                mixedColor.lerp(colorOutside, radius / parameters.radius)
-
-                
-
-                const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
-                const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
-                const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
-
-                positions[i3] = Math.cos(branchAngle + spinAngle) * radius + randomX
-                positions[i3 + 1] = randomY
-                positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ
-
-
-                colors[i3    ] = mixedColor.r
-                colors[i3 + 1] = mixedColor.g
-                colors[i3 + 2] = mixedColor.b
-            }
-
-            geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
-            geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
-
-            /**
-             * Material
-             */
-            material = new THREE.PointsMaterial({
-                size: parameters.size,
-                sizeAttenuation: true,
-                depthWrite: false,
-                blending: THREE.AdditiveBlending,
-                vertexColors: true
-            });
-
-            /**
-             * Points
-             */
-            points = new THREE.Points(geometry, material);
-            scene.add(points);
-        };
-
-        generateGalaxy();
-
-
-
-
-
-
-        // gui
-        gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
-        gui.add(parameters, 'size').min(0.001).max(0.1).step(0.001).onFinishChange(generateGalaxy)
-        gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
-        gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
-        gui.add(parameters, 'spin').min(-2).max(5).step(0.001).onFinishChange(generateGalaxy)
-        gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
-        gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
-        gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
-        gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
-
-        /**
-         * Sizes
-         */
-        const sizes = {
-            width: window.innerWidth,
-            height: window.innerHeight,
-        };
-
-        window.addEventListener('resize', () => {
-            // Update sizes
-            sizes.width = window.innerWidth;
-            sizes.height = window.innerHeight;
-
-            // Update camera
-            camera.aspect = sizes.width / sizes.height;
-            camera.updateProjectionMatrix();
-
-            // Update renderer
-            renderer.setSize(sizes.width, sizes.height);
-            renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-        });
-
-        /**
-         * Camera
-         */
-        // Base camera
-        const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
-        camera.position.z = 3;
-        scene.add(camera);
-
-        // Controls
-        const controls = new OrbitControls(camera, canvas);
-        controls.enableDamping = true;
-
-        /**
-         * Renderer
-         */
-        const renderer = new THREE.WebGLRenderer({
-            canvas: canvas,
-        });
-        renderer.setSize(sizes.width, sizes.height);
-        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-        /**
-         * Animate
-         */
-        const clock = new THREE.Clock();
-
-        const tick = () => {
-            const elapsedTime = clock.getElapsedTime();
-
-            // Update controls
-            controls.update();
-
-            // Render
-            renderer.render(scene, camera);
-
-            // Call tick again on the next frame
-            window.requestAnimationFrame(tick);
-        };
-
-        tick();
+    const previouslyCreatedSmoother = ScrollSmoother.get(); 
+    /**
+     * Debug
+     */
+    const gui = new dat.GUI()
+    
+    const parameters = {
+        materialColor: '#ffeded'
     }
+    
+    gui
+        .addColor(parameters, 'materialColor')
+        .onChange(() =>
+        {
+            material.color.set(parameters.materialColor)
+            particlesMaterial.color.set(parameters.materialColor)
+        })
+    
+    /**
+     * Base
+     */
+    // Canvas
+    const canvas = document.querySelector('canvas.js-scene')
+    
+    // Scene
+    const scene = new THREE.Scene()
+    
+    /**
+     * Objects
+     */
+    // Texture
+    const textureLoader = new THREE.TextureLoader()
+    const gradientTexture = textureLoader.load('/assets/textures/gradients/3.jpg')
+    gradientTexture.magFilter = THREE.NearestFilter
+    
+    // Material
+    const material = new THREE.MeshToonMaterial({
+        color: parameters.materialColor,
+        gradientMap: gradientTexture
+    })
+    
+    // Objects
+    const objectsDistance = 4
+    const mesh1 = new THREE.Mesh(
+        new THREE.TorusGeometry(1, 0.4, 16, 60),
+        material
+    )
+    const mesh2 = new THREE.Mesh(
+        new THREE.ConeGeometry(1, 2, 32),
+        material
+    )
+    const mesh3 = new THREE.Mesh(
+        new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+        material
+    )
+    
+    mesh1.position.x = 2
+    mesh2.position.x = - 2
+    mesh3.position.x = 2
+    
+    mesh1.position.y = - objectsDistance * 0
+    mesh2.position.y = - objectsDistance * 1
+    mesh3.position.y = - objectsDistance * 2
+    
+    scene.add(mesh1, mesh2, mesh3)
+    
+    const sectionMeshes = [ mesh1, mesh2, mesh3 ]
+    
+    /**
+     * Lights
+     */
+    const directionalLight = new THREE.DirectionalLight('#ffffff', 1)
+    directionalLight.position.set(1, 1, 0)
+    scene.add(directionalLight)
+    
+    /**
+     * Particles
+     */
+    // Geometry
+    const particlesCount = 200
+    const positions = new Float32Array(particlesCount * 3)
+    
+    for(let i = 0; i < particlesCount; i++)
+    {
+        positions[i * 3 + 0] = (Math.random() - 0.5) * 10
+        positions[i * 3 + 1] = objectsDistance * 0.5 - Math.random() * objectsDistance * sectionMeshes.length
+        positions[i * 3 + 2] = (Math.random() - 0.5) * 10
+    }
+    
+    const particlesGeometry = new THREE.BufferGeometry()
+    particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+    
+    // Material
+    const particlesMaterial = new THREE.PointsMaterial({
+        color: parameters.materialColor,
+        sizeAttenuation: textureLoader,
+        size: 0.03
+    })
+    
+    // Points
+    const particles = new THREE.Points(particlesGeometry, particlesMaterial)
+    scene.add(particles)
+    
+    /**
+     * Sizes
+     */
+    const sizes = {
+        width: window.innerWidth,
+        height: window.innerHeight
+    }
+    
+    window.addEventListener('resize', () =>
+    {
+        // Update sizes
+        sizes.width = window.innerWidth
+        sizes.height = window.innerHeight
+    
+        // Update camera
+        camera.aspect = sizes.width / sizes.height
+        camera.updateProjectionMatrix()
+    
+        // Update renderer
+        renderer.setSize(sizes.width, sizes.height)
+        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    })
+    
+    /**
+     * Camera
+     */
+    // Group
+    const cameraGroup = new THREE.Group()
+    scene.add(cameraGroup)
+    
+    // Base camera
+    const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+    camera.position.z = 6
+    cameraGroup.add(camera)
+    
+    /**
+     * Renderer
+     */
+    const renderer = new THREE.WebGLRenderer({
+        canvas: canvas,
+        alpha: true
+    })
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    
+    /**
+     * Cursor
+     */
+    const cursor = {}
+    cursor.x = 0
+    cursor.y = 0
+    
+    window.addEventListener('mousemove', (event) =>
+    {
+        cursor.x = event.clientX / sizes.width - 0.5
+        cursor.y = event.clientY / sizes.height - 0.5
+    })
+    
+    /**
+     * Animate
+     */
+    const clock = new THREE.Clock()
+    let previousTime = 0
+    
+    const tick = () =>
+    {
+        const elapsedTime = clock.getElapsedTime()
+        const deltaTime = elapsedTime - previousTime
+        previousTime = elapsedTime
+    
 
-    init();
+    
+        const parallaxX = cursor.x * 0.5
+        const parallaxY = - cursor.y * 0.5
+        cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime
+        cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
+    
+        // Animate meshes
+        for(const mesh of sectionMeshes)
+        {
+            mesh.rotation.x += deltaTime * 0.1
+            mesh.rotation.y += deltaTime * 0.12
+        }
+    
+        // Render
+        renderer.render(scene, camera)
+    
+        // Call tick again on the next frame
+        window.requestAnimationFrame(tick)
+    }
+    
+    tick()
+
+
+    initPin();
+
+
+
+    function initPin() {
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: '.js-scene',
+                start: `top top`,
+                scrub: true,
+                end: '400%',
+                pin: true,
+                pinType: 'transform',
+            },
+        });
+
+        gsap.to(camera.position, {
+            scrollTrigger: {
+                trigger: element,
+                start: 'top top',
+                end: '400%',
+                scrub: true,
+            },
+
+            y: -10,
+        });
+
+        // gsap.to(camera.position,
+
+        //     {
+        //         scrollTrigger: {
+        //             trigger: '.js-scene',
+        //             start: `top top`,
+        //             scrub: true,
+        //             end: `100%`, //to fix overlay on mobile
+    
+        //         },
+        //         y: previouslyCreatedSmoother.scrollTop
+        //     })
+    }
 };
